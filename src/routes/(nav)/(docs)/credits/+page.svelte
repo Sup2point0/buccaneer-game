980b0791524879ba1069e27e4d100f1a2a35bf0f
Sup#2.0 <sup2point0@gmail.com>
2025-04-10 18:30:41 +0100

    doc: add credits

---
+++ src/routes/(nav)/(docs)/credits/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+
+import { current } from "#src/version.json" assert { type: "json" };
+
+import Content from "./credits.svx";
+
+</script>
+
+<article>
+  <header>
+    <h1> Buccaneer Game </h1>
+    <p class="caption"> version {current} </p>
+  </header>
+
+  <main>
+    <Content />
+  </main>
+</article>
+
+
+<style>
+
+main {
+  text-align: center;
+}
+
+</style>
